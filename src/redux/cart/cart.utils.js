dcc32814a3cad2d6108c08be3e3c39d7b9ba0ca6
Shivam Chopra <shiva@LAPTOP-SRHNVCDJ.local>
2020-07-09 00:42:13 +0530

    Created Cart-item comonent and cart dropdown with cartItems reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,9 +4,9 @@ export const addItemTocart = (cartItems , cartItemToAdd) => {
     if(existingCartItem){
         return cartItems.map(cartItem => cartItem.id === cartItemToAdd.id 
             ? {...cartItem,
-                quanity : cartItem.quanity + 1
+                quantity : cartItem.quantity + 1
             } : cartItem);
     };
 
-    return [...cartItems,{...cartItemToAdd , quanity : 1}];
+    return [...cartItems,{...cartItemToAdd , quantity : 1}];
 };
