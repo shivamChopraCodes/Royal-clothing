60f92a4baa80e68e25412301d2506400bb59b3ff
Shivam Chopra <shiva@LAPTOP-SRHNVCDJ.local>
2020-07-08 15:56:59 +0530

    added redux,and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
 
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
 import "./App.css";
 
-function App() {
-  const [currentUser, setCurrentUser] = useState(null);
+function App({setCurrentUser}) {
+
 
   let unsubscribeFromAuth = null;
-
   useEffect(() => {
     unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -42,7 +44,7 @@ function App() {
 
   return (
     <div>
-      <Header currentUser={currentUser} />
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
@@ -52,4 +54,8 @@ function App() {
   );
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
