d8668c2b462aff268c7a6d02917d97861ce1dcb2
Shivam Chopra <shiva@LAPTOP-SRHNVCDJ.local>
2020-07-09 14:51:02 +0530

    added checkout page , checkout-itrm component and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import React, { useEffect } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
+import { createStructuredSelector } from "reselect";
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from "./redux/user/user.selector";
 
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from './pages/checkout/checkout.component';
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
@@ -48,6 +51,7 @@ function App({ currentUser, setCurrentUser }) {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
         <Route exact path="/signin" render={() => currentUser
           ?
           (<Redirect to='/' />)
@@ -59,8 +63,8 @@ function App({ currentUser, setCurrentUser }) {
   );
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -1,5 +1,6 @@
 import { createSelector } from "reselect";
 
+
 const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
@@ -7,7 +8,17 @@ export const selectCartItems = createSelector(
     cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+);
+
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
-   cartItems=> cartItems.reduce(((acc , cartItem)=> acc+ cartItem.quantity ),0)
+   cartItems=> cartItems.reduce(((acc , cartItem)=> acc + cartItem.quantity ),0)
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+   cartItems=> cartItems.reduce(((acc , cartItem)=> acc + cartItem.quantity * cartItem.price ),0)
 )
