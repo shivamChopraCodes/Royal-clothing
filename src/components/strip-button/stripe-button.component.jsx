b9db30162d036229bc11b2a083255f0be85892f1
Shivam Chopra <shiva@LAPTOP-SRHNVCDJ.local>
2020-07-10 00:13:39 +0530

    updating stripe integration and updating favicon and title

---
+++ src/components/strip-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from "react-stripe-checkout";
+import { renderIntoDocument } from 'react-dom/test-utils';
+
+const StripeCheckoutButton = ({ price }) =>{
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51H3462CMAxFdJJrI3jJg31932afPuXtrLdWlcNkZDxOU3oNmklILRmwNuiB8jUwphPoItTwq134tLdx8XBSWZhEI00pk4UpFD3';
+    const onToken = token => {
+        console.log(token);
+        alert("Payment Successfull");
+    }
+    return (
+        <StripeCheckout
+        label='Pay Now'
+        name='CRWN Clothing Ltd.'
+        billingAddress
+        shippingAddress
+        image='https://sendeyo.com/up/d/f3eb2117da'
+        description={`Yout total is $${price}`}
+        amount = {priceForStripe}
+        panelLabel='Pay Now'
+        token={onToken}
+        stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,7 +4,9 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector';
 import CheckoutItem from '../../components/checkout-item/checkout-item.components';
+import StripeCheckoutButtons from '../../components/strip-button/stripe-button.component';
 import './checkout.styles.scss';
+import StripeCheckoutButton from '../../components/strip-button/stripe-button.component';
 
 const CheckoutPage = ({cartItems , total}) => (
     <div className='checkout-page'>
@@ -28,6 +30,13 @@ const CheckoutPage = ({cartItems , total}) => (
         <div className='total'>
             <span>Total: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the test credit card for payments*
+            <br />
+            4242 4242 4242 4242
+            EXP: 01/21- CVV:123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
